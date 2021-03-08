25fbfb1f5563fd97b3fff94676f52a09a5ada8eb
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-08 20:36:09 +0100

    Tentative   infructieuse

---
+++ jour01/job09/script.js
@@ -0,0 +1,46 @@
+
+const numbers = [1,2,3,4,5]
+
+function  tri (numbers, order){
+const oder =  order
+
+    if(order === "asc"){
+        numbers.sort
+    }
+
+    if (order ===  "desc"){
+
+    }
+
+}
+
+
+
+//deux boucle. une pour le bas une pour le haut .  elles augmente toute deux de 1 en 1 en même temps
+
+//Si le chiffre au millieu est plus grand que le chiffre de la boucle et plus petit que la 2 e
+
+//alors on le push dans le tableaux
+
+
+
+
+function asc(chif) {
+    for (i = 0; i < chiff; i++) {
+
+        for (u = 2; u > chiff; u++) {
+        }
+
+
+    }
+}
+
+i = 0
+if (i < chiff){
+    i++
+}
+
+o = 2
+if (o > chiff){
+    i++
+}
