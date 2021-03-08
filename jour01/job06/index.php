125e0f07f9247cd441be71944fc5aab2b9abe51d
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-08 14:06:57 +0100

    Les chiffres jusqu'à 151 sont bien écrit dans la console

---
+++ jour01/job06/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index.php</title>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+<script>
+    fizzbuzz()
+</script>
+
+</body>
+</html>
---
+++ jour01/job06/script.js
@@ -0,0 +1,6 @@
+function fizzbuzz()
+{
+    for (i = 1; i < 152; i++){
+        console.log(i);
+    }
+}
