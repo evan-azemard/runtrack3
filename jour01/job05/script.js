c4b29f22109507517bbb1d7409974e31b868f72e
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-08 13:53:25 +0100

    Ajout d'un saut à la ligne

--- jour01/job05/script.js
+++ jour01/job05/script.js
@@ -3,7 +3,10 @@ function afficherjourssemaines()
     //Le tableaux qui contient les jours.
     var jourssemaines = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
     for (i = 0; i < 7; i++) //i servira d'index au tableaux.
+
     {
-        document.write(jourssemaines[i]);
+            const esp = '<br>';
+
+        document.write(jourssemaines[i] + esp);
     }
 }
