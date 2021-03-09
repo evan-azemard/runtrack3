a4ef7872b34218e52f41e39780d4906e904eb540
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-09 11:00:04 +0100

    Job01 foncitonne

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -5,6 +5,9 @@
     <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-
+<article id="citation">
+<p>"La vie a beaucoup plus d’imagination que nous”.</p>
+</article>
+<button id="button"onclick="citation()"></button>  <!--Si c'est cliqué sur le button -->
 </body>
 </html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,11 @@
+
+
+function citation(){
+    //On récupére le contenue de l'article
+    var text = document.getElementById('citation').textContent;
+
+    console.log(text) //On envoie sur la console
+}
+
+
+
