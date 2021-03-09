95b39b6cddd97da486a3bc2e33bc4fc1882f6284
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-09 12:11:34 +0100

    Le job fonctionne

---
+++ jour02/job02/script.js
@@ -0,0 +1,43 @@
+
+const article = "<article><p>“L'important n'est pas la chute, mais l'atterrissage.”</p></article>";
+
+const article2 = "<article><p>“L'important n'est pas la chute, mais l'atterrissage.!!”</p></article>";
+
+
+
+const but = "<button style='background-color: red' onclick='javascript:location.reload();'> </button>"
+
+
+
+
+function showhide(){
+    document.write(article)
+    document.write(but)
+}
+
+function hide(){
+    document.write(article2)
+}
+
+
+
+
+//Si tu clique sur le button
+//On envoie le texte
+//On envoie un nouveaux button
+
+//Si tu clique sur le nouveaux button
+//On supprime le text
+//On envoie le premier button
+
+
+
+//Le button à une valu="1"
+
+//Si tu clique tu augmente la value de plus 1
+
+//Si la valeur est différente  d'une boucle qui augemente aussi à chaque clique mais qui est un chiffre plus petit que le button
+
+//
+
+
