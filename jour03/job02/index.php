08aa5a401948131e107b56e3f2df1a8b262924c9
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-10 10:31:41 +0100

    amélioration script js

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -8,6 +8,11 @@
 </head>
 <body>
 
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
 </body>
 </html>
-<script src="script.js"></script>
