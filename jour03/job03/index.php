5143389869629408165341d9e1e86b37099dfbaa
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-10 18:05:15 +0100

    Grosse diffuculté. Code commenté.Le code ne m'apartient pas .

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Runtrack 3 - Jour 2 - Job 6</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
 
-</body>
+
+<html>
+	<head>
+		<title></title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	</head>
+	<body style="display:flex;flex-wrap:wrap;max-height:450px;max-width:500px;">
+		
+		<script src="script.js"></script>
+	</body>
 </html>
-<script src="script.js"></script>
