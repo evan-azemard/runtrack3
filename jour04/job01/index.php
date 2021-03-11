4eb83515cf56aa679c4caea51a7dff555aad3b8c
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-11 11:16:57 +0100

    Piste ajax

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </head>
 <body>
 
+<button id="button"></button>
 
 
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript" src="script.js"></script>
 
 </body>
---
+++ jour04/job01/script.js
@@ -0,0 +1,32 @@
+$('#button').click(function (){
+
+
+
+
+});
+
+var a;
+a = $.ajax({
+    url: 'expression.txt',
+    type: 'POST',
+    datatype: 'text',
+
+
+})
+
+var b =    $('#id_objet-a-remplir').text(expression)
+
+
+
+
+
+document.write(b)
+
+
+
+/*
+document.write(test)
+*/
+/*
+var test = <object data="expression.txt">;
+*/
