f17cbaf9db68bd4e461a7fd8ce0b092f7950eeb4
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-11 16:45:59 +0100

    Très proche de marcher parfaitement

--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -2,20 +2,24 @@
 
 $("#filtrer").click(function() {
 
-                var id = $("#id").val().toString();
+                var id = $("#id").val().toString()-1;
                 var name = $("#name").val().toString();
                  $.ajax({
                      type: "GET", url: "pokemon.json", dataType: "text",
                      success: function (data) {
 
                          res = JSON.parse(data);
+                            //Si c'est un object on transpforme en tableaux
+                        /* $('body').append("<p>"+res[id][name]+"</p>");*/
 
-                         $('body').append("<p>"+res[id][name]+"</p>");
+                         var tab  = [];
+
+                         tab.push(res[id][name])
+
+                          $('body').append("<p>"+tab+"</p>");
                      }
-
              })
     })
-
 
 
 /*
