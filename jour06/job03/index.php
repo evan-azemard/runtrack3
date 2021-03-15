2ae553039852db13dab8e90ac6b9d4484bedd627
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-15 11:53:30 +0100

    Le css marche manque les condition js

--- jour06/job03/index.php
+++ jour06/job03/index.php
@@ -6,9 +6,21 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </head>
-<body>
+<body class="body">
 
-<h1>Exo 3</h1>
+
+
+<div class="droit">
+    <div class="deuxlignes droit">
+        <section class="section"> section 1 </section>
+        <section class="section"> section 2 </section>
+    </div>
+    <div class="deuxlignes droit">
+        <section class="section"> section 3 </section>
+        <section class="section"> section 4 </section>
+    </div>
+</div>
+
 
 </body>
 </html>
