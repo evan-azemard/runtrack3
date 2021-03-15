f6f1bc6aa98f6cfb9cbf086b602589fb7c0fb306
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-15 10:58:12 +0100

    Job02 fonctionne

---
+++ jour06/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</head>
+<body>
+
+<h1>Exo 2</h1>
+
+</body>
+</html>
