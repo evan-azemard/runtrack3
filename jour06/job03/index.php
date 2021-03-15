f3d85217a52564b367d80d62c17d2235366ebd9c
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-15 15:15:25 +0100

    Test qui marche en parti

--- jour06/job03/index.php
+++ jour06/job03/index.php
@@ -6,16 +6,15 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </head>
+
 <body class="body">
 
-
-
-<div class="droit">
-    <div class="deuxlignes droit">
+<div id="droit">
+    <div  class="dd">
         <section class="section"> section 1 </section>
         <section class="section"> section 2 </section>
     </div>
-    <div class="deuxlignes droit">
+    <div class="dd">
         <section class="section"> section 3 </section>
         <section class="section"> section 4 </section>
     </div>
@@ -24,3 +23,64 @@
 
 </body>
 </html>
+
+    <script>
+
+
+
+
+        //   Si c plus petit que 1599px  on atribut du flex pour faire 2 lignes
+
+
+
+
+    /*    function myFunction(width){
+            if (width.matches){
+                $(".dd").css("display", "flex");
+            }
+        }
+
+        var width = window.matchMedia("(max-width: 1599px)");
+
+        myFunction(width)
+
+        width.addListener(myFunction)*/
+
+
+         // Si c plus petit que 768 = une colone
+
+
+       /* function myFunctionA(ligne){
+            if (ligne.matches){
+                $(".dd").css("display","flex");
+                $("#droit").css("flex-direction","column");
+
+            }
+        }
+
+        var ligne = window.matchMedia("(min-width: 767px)");
+
+        myFunctionA(ligne)
+
+        ligne.addListener(myFunctionA)
+
+
+
+
+
+         function myFunctionAB(double){
+            if (double.matches){
+                $(".dd").css("display","flex");
+                $("#droit").css("display","flex");
+
+            }
+        }
+
+        var double = window.matchMedia("(min-width: 769px)");
+
+        myFunctionAB(double)
+
+        double.addListener(myFunctionAB)
+*/
+
+    </script>
