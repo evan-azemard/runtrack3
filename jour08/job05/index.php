e379339c4edf26e49fc3f51b4fc8318990ea15b3
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-03-17 13:05:14 +0100

    OK fonctionne

---
+++ jour08/job05/index.php
@@ -0,0 +1,154 @@
+  <!DOCTYPE html>
+  <html lang="fr">
+    <head>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+    <header>
+        <nav>
+            <div class="nav-wrapper" style="display: flex; justify-content: center">
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="#">Ticket</a></li>
+                    <li><a href="#">Horaires</a></li>
+                    <li><a href="#">Décolage</a></li>
+                </ul>
+                <i class="large material-icons" style="margin-left: 2%">airplanemode_active</i>
+
+            </div>
+        </nav>
+    </header>
+    <main>
+
+
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://th.bing.com/th/id/R0185dee5fe92772263fd868967e95270?rik=MRri6Eo%2f%2b0GocA&riu=http%3a%2f%2fi.huffpost.com%2fgen%2f4594324%2fimages%2fo-AVION-NASA-facebook.jpg&ehk=MNT39SJgrarYOyyvWcAKtM9Ji5Siio%2fSWAUF1XHpP5Q%3d&risl=&pid=ImgRaw">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Aurora Avia<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Informations</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+  <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="https://th.bing.com/th/id/Rb7860be314e5f1d1c7d033177b3ddd63?rik=lBDGzGWajk%2bAMw&riu=http%3a%2f%2fd1poh340f4imgl.cloudfront.net%2fupload%2fimages%2f534x326%2f2015%2f06%2f08%2f6d821f885be03809f147d15fbae63927_534x326.jpg&ehk=fJTZlxbXP76m8oLOPbnYkPAt5qcUeED2KcP9YX09joM%3d&risl=&pid=ImgRaw">
+        </div>
+        <div class="card-content">
+          <p>Conditions Générales de vente Politique de cookies Politique de confidentialité
+        *Tarifs « à partir de » et sous réserve de disponibilité :
+        - Vol : Prix TTC, « à partir de » et par personne, incluant les taxes d'aéroport, hors assurances optionnelles, frais de réservation, de livraison et frais de services, pour un vol aller-retour en classe économique, sur une sélection de dates et de destinations. Offre soumise à conditions et valable sous réserve de disponibilité et de confirmation de la compagnie aérienne.
+        - Vol + Hôtel : Prix TTC, « à partir de » et par personne, incluant les taxes d'aéroport, hors assurances optionnelles, frais de réservation, de livraison, frais de services et taxes locales (Resort Fees) pour un vol aller-retour en classe économique, sur la base d'une chambre double, sur une sélection de dates et de destinations. Sous réserve de disponibilité et de confirmation.
+        - Train + Hôtel : Prix TTC, « à partir de » et par personne, incluant les taxes, hors assurances optionnelles, frais de réservation, de livraison et frais de services, pour un train aller-retour en seconde classe, sur la base d'une chambre double, sur une sélection de dates. Offre soumise à conditions et valable sous réserve de disponibilité et de confirmation.
+        - Séjour / Thalasso : Prix de référence du fournisseur TTC, « à partir de » et par personne, incluant les taxes aéroport, hors suppléments, assurances optionnelles, frais de réservation, de livraison et de services, pour un vol aller-retour en classe économique, sur la base d'une chambre double, incluant les transferts aéroport/hôtel/aéroport (sauf mention contraire), sur une sélection de dates et de destinations. Pour les séjours en France, les prix n'incluent pas le vol et le transfert. Offre soumise à conditions et valable sous réserve de confirmation et de disponibilité par le fournisseur.
+        - Hôtel : Prix TTC, « à partir de », par chambre, offre soumise à conditions et valable sous réserve de disponibilité et de confirmation du prestataire, hors taxes locales (Resort Fees).- Voiture : Prix « à partir de ». Offre soumise à conditions et valable sous réserve de disponibilité et de confirmation du prestataire pour un modèle identique ou similaire.
+        - Croisière : Prix « à partir de », par personne, base occupation double de la cabine, pour la date sélectionnée. Offre valable soumise à conditions et valable sous réserve de disponibilité et de confirmation du fournisseur.
+        - Location / Ski /Camping : Prix « à partir de », par semaine et par logement, hors frais de dossiers et taxes de séjour.Offre soumise à conditions et valable sous réserve de disponibilité et de confirmation du fournisseur.
+        Comparaison des vols effectuée avec le tarif de base (hors compléments) pour le même numéro de vol. Comparaison mensuelle portant sur 20 000 vols en moyenne. Comparaison des hôtels effectuée avec les tarifs de base pour la même chambre d'hôtel et les mêmes dates. Hôtels dans les 25 premières destinations et les 20 premiers résultats de recherche. Prix le plus bas 9 fois sur 10 en moyenne pour les vols et les hôtels. Dans le cas peu probable où vous trouveriez un prix moins cher, nous offrons notre Garantie du meilleur prix.
+        © 1999-2021 GO Voyages. Tous droits réservés - Vacaciones eDreams, S.L. société soumise au droit espagnol, inscrite au registre du commerce de Madrid, Tomo 36897, Folio 121, Hoja M-660117 dont le siège social est Calle Conde de Peñalver 5, 1 Ext. Izq., 28006, Madrid, Espagne immatriculée au Registre des opérateurs de voyages et de séjours auprès de Atout France sis 79/81 rue de Clichy 75009 Paris, sous le numéro IM099170015, titulaire d'une police d'assurance auprès de Hiscox, sis Paseo de la Castellana, 60, 7ª Planta, 28046 Madrid, Espagne, agréée IATA. - Contact : customerservice-fr@contact.govoyages.com.
+        Si vous avez soumis une réclamation auprès de notre Service Client, mais que notre réponse ne vous satisfait pas : vous pouvez contacter la Médiation Tourisme et Voyage sur leur site www.mtv.travel ou par voie postale MTV Médiation Tourisme Voyage BP 80 303 75 823 Paris cedex 17. Vous pouvez nous contacter au sujet de votre remboursement dès maintenant via notre Centre d'aide, ou en envoyant un courrier à l'adresse Vacaciones eDreams, S.L., Carrer de Bailèn 67-69, 08009 Barcelona, Espagne.
+    </p>
+        </div>
+        <div class="card-action">
+          <a href="#">Ou vont m'es données ?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+        <p>
+    </div>
+  </div>
+        <section style="height: 100vh; display: flex; flex-direction: column; text-align: center; align-items: center"  >
+            <article style="width: 50vw">
+
+              <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="https://th.bing.com/th/id/R0ac0e00760fc2887ad07f7b7eedde7fc?rik=XX0BhCiyCnLNXA&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2ftravel%2ftravel-14.jpg&ehk=rUvNvuqS4fJ28PY3hv7B9gC20n%2bkEPn6DUXtarVY5r4%3d&risl=&pid=ImgRaw">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator grey-text text-darken-4">Reserver içi<i class="material-icons right">more_vert</i></span>
+                </div>
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4">Reserver içi <i class="material-icons right">close</i></span>
+                    <p>
+                      <div class="collection">
+                        <a href="#!" class="collection-item"><span class="badge">1</span>France</a>
+                        <a href="#!" class="collection-item"><span class="new badge">4</span>Australie</a>
+                        <a href="#!" class="collection-item"><span class="new badge">4</span>Belgique</a>
+                        <a href="#!" class="collection-item"><span class="badge">14</span>Espagne</a>
+                      </div>
+                     </p>
+                </div>
+              </div>
+
+
+            </article>
+        <div style="height: 30vh; width: 100vh; display: flex; justify-content: space-between;">
+
+                <div  style="width: 15vw">
+                    <div class="col s12 m6">
+                      <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                          <span class="card-title">Nos tarifs</span>
+                              <p>Lorem to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or</p>
+                        </div>
+                        <div class="card-action">
+                          <a href="#">This is a link</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+                <div  style="width: 15vw">
+                    <div class="col s12 m6">
+                      <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                          <span class="card-title">Card Title</span>
+                          <p>Lorem to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or</p>
+                        </div>
+                        <div class="card-action">
+                          <a href="#">This is a link</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+             <div   style="width: 15vw">
+                    <div class="col s12 m6">
+                      <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                          <span class="card-title">Card Title</span>
+                          <p>Lorem to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or</p>
+                        </div>
+                        <div class="card-action">
+                          <a href="#">This is a link</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+
+
+        </div>
+
+
+        </section>
+
+
+
+
+    </main>
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
